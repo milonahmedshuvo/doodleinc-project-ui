e3a947060ed3ef78e8bf25128719d345afaf4a76
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-01-29 00:17:24 +0600

    comment delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { router } from './layout/routes';
 import toast, { Toaster } from 'react-hot-toast';
 
 
+
 function App() {
   
 
@@ -13,6 +14,7 @@ function App() {
       
       <RouterProvider router={router} ></RouterProvider>
        <Toaster/>
+      
     </div>
   )
 }
--- src/components/blogpage/Blogdatails.jsx
+++ src/components/blogpage/Blogdatails.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 import { useLoaderData, useParams } from 'react-router-dom'
 
 const Blogdatails = () => {
-    const ids = useParams()
+    
+    let {id:blogId} = useParams()
     const blog = useLoaderData()
     const {id, title, body} = blog
+
+    console.log("datails blog id", typeof blogId)
+
+    const {data, refetch, isLoading } = useQuery({
+        queryKey: [],
+        queryFn: async () => {
+           const res =await fetch(`http://localhost:5000/comment/${blogId}`)
+           const result = await res.json()
+           return result
+        }
+    })
+          if(isLoading){
+            return <p>Loading...........</p>
+          }
+
+          
+    
+    console.log("committttttttttt", data)
+
 
     const handleCommentpost = (event) => {
         event.preventDefault()
@@ -12,7 +34,48 @@ const Blogdatails = () => {
         const email = event.target.email.value;
         const comment = event.target.comment.value;
         console.log(name, email, comment)
+        const commentData = {
+            blogId: id,
+            name, 
+            email, 
+            body: comment
+        }
+      
+        fetch("http://localhost:5000/commentpost", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(commentData)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            toast.success("Succesfully comment!")
+            console.log(data)
+            refetch()
+        })
+        .catch((err)=> {
+            console.log(err)
+            toast.error('fail comment!')
+        })
     }
+
+
+
+
+     const handleCommentDelete = (id) =>{
+        console.log(id)
+        fetch(`http://localhost:5000/commentDelete/${id}`, {
+            method: "DELETE",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            toast.success("Succesfully comment delete!")
+            refetch()
+        })
+        .catch((err) => console.log(err))
+     }
+
   return (
     <div className='my-14'>
       <h1 className='lato my-3'> {title} </h1>
@@ -30,6 +93,29 @@ const Blogdatails = () => {
         <input type="text" name="comment" required placeholder='Comment' id="" className='border border-gray-300 py-2 rounded  pl-2 w-full mx-auto outline-none' />
         <input type="submit" value="Comment" className='bg-black text-white font-semibold px-4 py-1 mt-2 ' />
         </form>
+      </div>
+
+
+
+      {/* all comment show in the page  */}
+      <div className='mt-5'>
+        {
+            data?.map((comment, i) => <div key={i} className='border border-gray-300 px-2 py-4 my-5 w-full md:w-3/6 lg:w-2/6 rounded'>
+
+                 
+                 <p>{comment.name}</p>
+                 <p>{comment.email}</p>
+                 <p>{comment.body}</p>
+
+
+                 <div className='flex justify-between items-center mt-5'>
+                 
+                 <input type="button" value="Update" className='bg-green-600 text-white font-normal py-1 px-4 rounded' />
+
+                 <input type="button" onClick={()=> handleCommentDelete(comment._id)}  value="Delate" className='bg-red-400 text-white font-normal py-1 px-4 rounded' />
+                 </div>
+            </div>)
+        }
       </div>
     </div>
   )
